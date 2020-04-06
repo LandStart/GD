266d9ccf388ab435a1faeb0062d2f45dbfd06294
LandStart <320514639@qq.com>
2020-04-06 10:09:02 +0800

    ”基本功能开发完毕

--- graduate/src/main/java/com/dong/graduate/GraduateApplication.java
+++ graduate/src/main/java/com/dong/graduate/GraduateApplication.java
@@ -2,14 +2,22 @@ package com.dong.graduate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement(proxyTargetClass = true)
-public class GraduateApplication {
+public class GraduateApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GraduateApplication.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
